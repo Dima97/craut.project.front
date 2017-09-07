cafce1173133586183adb06f72c3bf48d5e2d746
Dima_Apanasik <dima.apanasik@yandex.ru>
2017-09-07 15:34:04 +0300

    link in modal window work!!!!)))

--- src/app/component/LoginPage/login.component.ts
+++ src/app/component/LoginPage/login.component.ts
@@ -1,5 +1,6 @@
 import {animate, Component, state, style, transition, trigger} from '@angular/core';
 
+declare var $:any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,5 +8,9 @@ import {animate, Component, state, style, transition, trigger} from '@angular/co
 
 })
 export class LoginComponent {
+  activeLink: boolean = false;
 
+  changeActive(){
+    this.activeLink = !this.activeLink;
+  }
 }
